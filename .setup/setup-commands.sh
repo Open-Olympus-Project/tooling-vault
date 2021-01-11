126fd0770efb485ac7bd85c431920ac0988d4556
MadsHT <31923273+MadsHT@users.noreply.github.com>
2021-01-11 11:37:00 +0100

    Update setup-commands.sh

--- .setup/setup-commands.sh
+++ .setup/setup-commands.sh
@@ -24,7 +24,7 @@ vault auth enable kubernetes
 vault write auth/kubernetes/config token_reviewer_jwt="$(cat /var/run/secrets/kubernetes.io/serviceaccount/token)" kubernetes_host="https://$KUBERNETES_PORT_443_TCP_ADDR:443" kubernetes_ca_cert=@/var/run/secrets/kubernetes.io/serviceaccount/ca.crt
 
 vault policy write k8s-secrets - <<EOF
-path "k8s/data/secrets/github-ssh" {
+path "k8s/*" {
 	capabilities = ["read"]
 }
 EOF
