5e1139fdc2474b2e5765ce37cc37373e94bd9ca8
MadsHT <hrthyssen@gmail.com>
2021-07-08 15:13:14 +0200

    think I fixed it

--- vault/files/vault-config.sh
+++ vault/files/vault-config.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-authMethodName=Kubernetes
+authMethodName=kubernetes
 secretPath=k8s
 
 KUBE_TOKEN=$(cat /var/run/secrets/kubernetes.io/serviceaccount/token)
@@ -51,7 +51,7 @@ do
 	fi
 	for j in `seq 0 2`;
 	do
-		echo "Unsealing vault-$i";
+		echo "Unsealing vault-$i $j";
 		if vault operator unseal $(cat cluster-keys.json | jq -r ".unseal_keys_b64[$j]");
 		then 
 			sleep 10;
@@ -68,6 +68,9 @@ echo "Start working on enabeling the first KV-V2 store"
 VAULT_ADDR=http://$(wget -vO- -nv --ca-certificate /var/run/secrets/kubernetes.io/serviceaccount/ca.crt --header "Authorization: Bearer $KUBE_TOKEN" https://kubernetes.default/api/v1/namespaces/$namespace/pods/vault-0 | jq -r .status.podIP ):8200
 
 echo "Vault-0's address is $VAULT_ADDR"
+
+root_token=$(cat cluster-keys.json | jq -r '.root_token')
+vault login $root_token -no-print=true 1>/dev/null
 
 echo "Enabled secret engiene $secretPath"
 vault secrets enable -path=$secretPath kv-v2;
@@ -86,19 +89,19 @@ echo "Creating role for $secretPath"
 vault write auth/$authMethodName/role/$secretPath-secrets bound_service_account_names=tooling-prometheus-server,argo,argo-server,argocd-server,default bound_service_account_namespaces=monitoring,argo,argocd policies=$secretPath-secrets ttl=24h;
 
 echo "Fetching vault sa secret name"
-VAULT_SA_SECRET_NAME=$(wget -vO- -nv --ca-certificate /var/run/secrets/kubernetes.io/serviceaccount/ca.crt --header "Authorization: Bearer $KUBE_TOKEN" https://kubernetes.default/api/v1/namespaces/$namespace/serviceaccount/vault | jq -r -e '.secrets[] | select(.name | test("vault-token")) | .name');
+VAULT_SA_SECRET_NAME=$(wget -vO- -nv --ca-certificate /var/run/secrets/kubernetes.io/serviceaccount/ca.crt --header "Authorization: Bearer $KUBE_TOKEN" https://kubernetes.default/api/v1/namespaces/$namespace/serviceaccounts/vault | jq -r -e '.secrets[] | select(.name | test("vault-token")) | .name');
 
 echo "Getting token and crt"
 SA_JWT_TOKEN=$(wget -vO- -nv --ca-certificate /var/run/secrets/kubernetes.io/serviceaccount/ca.crt --header "Authorization: Bearer $KUBE_TOKEN" https://kubernetes.default/api/v1/namespaces/$namespace/secrets/$VAULT_SA_SECRET_NAME | jq -r -e '.data.token' | base64 -d; echo);
 SA_CA_CRT=$(wget -vO- -nv --ca-certificate /var/run/secrets/kubernetes.io/serviceaccount/ca.crt --header "Authorization: Bearer $KUBE_TOKEN" https://kubernetes.default/api/v1/namespaces/$namespace/secrets/$VAULT_SA_SECRET_NAME | jq -r -e '.data."ca.crt"' | base64 -d; echo);
 
 echo "Configuring auth path"
-vault write auth/$authMethodName/config token_reviewer_jwt=$SA_JWT_TOKEN kubernetes_host="https://kubernetes.default:443" kubernetes_ca_cert=$SA_CA_CRT
+vault write auth/$authMethodName/config token_reviewer_jwt="$SA_JWT_TOKEN" kubernetes_host="https://kubernetes.default:443" kubernetes_ca_cert="$SA_CA_CRT"
 
 echo "Putting the root_token into the vault"
-vault kv put k8s/secrets/vault root_token=$(cat cluster-keys.json | jq -r '.root_token');
+vault kv put k8s/secrets/vault root_token=$root_token;
 
 echo "Putting the cluster_keys into the vault"
-vault kv put k8s/secrets/vault cluster_keys=$(cat cluster-keys.json);
+vault kv put k8s/secrets/vault cluster_keys="$(cat cluster-keys.json)";
 
 while true; do sleep 10; done;
